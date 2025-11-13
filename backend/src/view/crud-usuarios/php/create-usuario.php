30d7dfee31f262c56446f75954399286d71b1659
Dayvson <dayvsonnlacerda360@gmail.com>
2025-11-13 15:45:54 -0300

    Erro consertado
    ajuste de endereçamento

--- backend/src/view/crud-usuarios/php/create-usuario.php
+++ backend/src/view/crud-usuarios/php/create-usuario.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt->execute([$nome, $email, $cpf, $telefone]);
     
     // Redireciona para a página de listagem de usuarios após a inserção
-    header('Location: index-aluno.php');
+    header('Location: index-usuario.php');
 }
 ?>
 
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adicionar aluno</title>
+    <title>Adicionar usuario</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
