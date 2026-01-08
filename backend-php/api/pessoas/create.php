2dd97150b96644c12f4d33be3ffd949545ac615e
Camilla Barros <camillaerbarros@gmail.com>
2026-01-08 17:36:18 -0300

    refactor: refatora o endpoit pessoa/create

--- backend-php/api/pessoas/create.php
+++ backend-php/api/pessoas/create.php
@@ -9,40 +9,51 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 
 $data = json_decode(file_get_contents("php://input"));
 
-// Validação dos campos NOT NULL do banco
+
 if(
-    !isset($data->nome) || 
-    // !isset($data->matricula) || 
-    !isset($data->cpf) || 
-    !isset($data->email)
+    empty($data->nome) || 
+    empty($data->matricula) || 
+    empty($data->cpf) || 
+    empty($data->email) ||
+    empty($data->tipo)
 ) {
-    enviarResposta("erro", "Dados incompletos. Informe nome, cpf e email.", null, 400);
+    enviarResposta("erro", "Dados incompletos. Nome, matrícula, cpf, email e tipo são obrigatórios.", null, 400);
 }
 
 try {
-    $sql = "INSERT INTO pessoa (nome, matricula , cpf, email, telefone) VALUES (?, ?, ?, ?, ?)";
+    
+    $sql = "INSERT INTO pessoa (nome, matricula, cpf, email, telefone, tipo, cargo) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)";
+    
     $stmt = $pdo->prepare($sql);
     
-    // Telefone e Tipo são opcionais no JSON, mas Tipo tem ENUM no banco
+ 
     $telefone = isset($data->telefone) ? $data->telefone : null;
-    $tipo = isset($data->tipo) ? $data->tipo : 'aluno'; // Default para aluno
+    $tipo = $data->tipo; 
+    
+    // Lógica para o Cargo
+    // Se não for funcionário, o cargo deve ser null
+    $cargo = ($tipo === 'funcionario' && isset($data->cargo)) ? $data->cargo : null;
 
     $stmt->execute([
         $data->nome, 
         $data->matricula, 
         $data->cpf, 
         $data->email, 
-        $telefone, 
+        $telefone,
+        $tipo,
+        $cargo
     ]);
 
-    enviarResposta("sucesso", "Pessoa cadastrada com sucesso!", ["id_pessoa" => $pdo->lastInsertId()], 201);
+    enviarResposta("sucesso", "Cadastro realizado com sucesso!", ["id_pessoa" => $pdo->lastInsertId()], 201);
 
 } catch (PDOException $e) {
-    // Erro 23000 = Violação de Unique (CPF, Email ou Matrícula repetidos)
+    
     if ($e->getCode() == '23000') {
-        enviarResposta("erro", "Já existe uma pessoa cadastrada com esse CPF, Matrícula ou Email.", null, 409);
+        enviarResposta("erro", "Já existe uma pessoa com este CPF, Matrícula ou Email cadastrado.", null, 409);
     } else {
-        enviarResposta("erro", "Erro no banco: " . $e->getMessage(), null, 500);
+        
+        enviarResposta("erro", "Erro no banco de dados: " . $e->getMessage(), null, 500);
     }
 }
 ?>
